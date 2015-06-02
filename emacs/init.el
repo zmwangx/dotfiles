52a1c36a7682e071ccccad1f173477b84074826a
Zhiming Wang <zmwangx@gmail.com>
2015-06-01 21:54:25 -0700

    add emacs configs
    Heavily based on use-package.

---
+++ emacs/init.el
@@ -0,0 +1,326 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; GLOBAL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; package.el (with MELPA and local packages)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(setq package-enable-at-startup nil)
+(eval-and-compile
+  (mapc #'(lambda (path) (push (expand-file-name path user-emacs-directory) load-path))
+        '("local" "local/use-package")))
+(package-initialize)
+
+;;; use-package init
+(eval-when-compile
+  (defvar use-package-verbose nil)
+  (require 'use-package))
+(require 'bind-key)
+(require 'diminish nil t)
+
+;;; editing and saving
+(setq-default auto-save-default nil)
+(setq-default indent-tabs-mode nil)
+(setq-default make-backup-files nil)
+(setq-default require-final-newline t)
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+;;; UI
+(setq-default inhibit-startup-screen t)
+(column-number-mode)
+
+;;; global keybindings
+(bind-key "C-\\" 'delete-trailing-whitespace)
+(bind-key "C-x C-k" 'server-edit)
+(bind-key "M-s" 'shell-command)
+(bind-key "C--" 'undo)
+
+;; enable
+(put 'downcase-region 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; GLOBAL THEMING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(if (not (window-system))
+    ;;; tty mode, colors based on my modified Solarized palatte for iTerm2
+    (progn
+      (setq mytheme 'solarized-dark)
+      (setq-default frame-background-mode 'dark)
+      (menu-bar-mode -1)
+
+      (set-face-attribute 'font-lock-builtin-face nil :foreground "blue")
+      (set-face-attribute 'font-lock-comment-face nil :foreground "yellow")
+      (set-face-attribute 'font-lock-constant-face nil :foreground "green")
+      (set-face-attribute 'font-lock-function-name-face nil :foreground "blue")
+      (set-face-attribute 'font-lock-keyword-face nil :foreground "cyan")
+      (set-face-attribute 'font-lock-string-face nil :foreground "green")
+      (set-face-attribute 'font-lock-type-face nil :foreground "cyan")
+      (set-face-attribute 'font-lock-variable-name-face nil :foreground "brightred")
+      (set-face-attribute 'region nil :background "brightcyan"))
+
+  ;;; window system mode
+  (progn
+    (setq mytheme 'default)
+    (setq-default frame-background-mode 'light)
+
+    ;; default face
+    (set-face-attribute 'default nil
+                        :family "Source Code Pro" :foundry "adobe"
+                        :width 'normal :height 120
+                        :weight 'normal :slant 'normal
+                        :foreground "black" :distant-foreground "black" :background "white"
+                        :underline nil :overline nil :strike-through nil :box nil :inverse-video nil
+                        :stipple nil :inherit nil)
+    ;; turn off antialiasing
+    ;(setq-default mac-allow-anti-aliasing nil)
+
+    ;; change font for Chinese fontset (han)
+    (set-fontset-font "fontset-default" 'han "STSong")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; GLOBAL FUNCTIONS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun indent-and-newline-and-indent ()
+  "Correct indentation of current line, insert newline, and indent the new line."
+  (interactive)
+  (indent-for-tab-command)
+  (newline-and-indent))
+
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil region)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BASIC MAJOR MODES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package prog-mode
+  :preface
+  (defun my-prog-mode-hook ()
+    (auto-complete-mode 1)
+    (hs-minor-mode 1)
+    (setq fill-column 79)
+    (bind-key "RET" 'indent-and-newline-and-indent prog-mode-map))
+
+  :config
+  (add-hook 'prog-mode-hook 'my-prog-mode-hook))
+
+;;; text-mode (which doesn't a feature, so can't use use-package with it)
+(defun my-text-mode-hook ()
+  (abbrev-mode 1))
+(add-hook 'text-mode-hook 'my-text-mode-hook)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; BUILTIN PACKAGES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package abbrev :commands abbrev-mode :diminish abbrev-mode :config
+  (setq abbrev-file-name "~/.emacs.d/local/abbrev_defs")
+  (if (file-exists-p abbrev-file-name) (quietly-read-abbrev-file)))
+
+(use-package auto-complete :commands auto-complete-mode :diminish auto-complete-mode)
+
+(use-package cc-mode
+  :preface
+  (defun my-c-mode-common-hook ()
+    (auto-fill-mode 1)
+    (setq-default c-basic-offset 4)
+    (unless (keymap-parent c-mode-base-map)
+      (set-keymap-parent c-mode-base-map prog-mode-map)))
+
+  :config
+  (add-hook 'c-mode-common-hook 'my-c-mode-common-hook))
+
+(use-package cperl-mode :mode "\\.pl\\'" :interpreter "perl" :config
+  (setq-default cperl-indent-level 4)
+  (if (equal mytheme 'solarized-dark)
+      (progn
+        (set-face-attribute 'cperl-array-face nil :foreground "red" :background "default" :weight 'bold :slant 'normal)
+        (set-face-attribute 'cperl-hash-face nil :foreground "red" :background "default" :weight 'bold :slant 'normal)
+        (set-face-attribute 'cperl-nonoverridable-face nil :foreground "cyan"))))
+
+(use-package make-mode :mode ("\\Makefile\\'" . makefile-mode) :config
+  (if (equal mytheme 'solarized-dark)
+      (set-face-attribute 'makefile-space nil :background "magenta")))
+
+(use-package python :mode ("\\.py\\'" . python-mode) :interpreter ("python" . python-mode)
+  :preface
+  (defun my-python-mode-hook ()
+    (setq-default fill-column 72)
+    ;; Python's lack of braces or ends makes it hard to deduce indentation automatically
+    (bind-key "RET" 'newline-and-indent python-mode-map)
+    ;; jedi
+    (setq-default jedi:complete-on-dot t)
+    (setq-default jedi:environment-root "jedi")
+    (jedi:setup))
+
+  :config
+  (add-hook 'python-mode-hook 'my-python-mode-hook))
+
+(use-package ruby-mode :mode "\\.rb\\'" :interpreter "ruby")
+
+(use-package sh-script :mode ("\\.zsh\\'" . sh-mode) :config
+  (if (equal mytheme 'solarized-dark)
+      (progn
+        (set-face-attribute 'sh-heredoc nil :foreground "brightyellow" :weight 'regular)
+        (set-face-attribute 'sh-quoted-exec nil :foreground "red"))))
+
+(use-package hideshow :commands hs-minor-mode :diminish hs-minor-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;; PACKAGE.EL PACKAGES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package adoc-mode :ensure t :mode "\\.adoc\\'")
+
+(use-package tex-site :ensure auctex :mode ("\\.tex\\'" . LaTeX-mode)
+  :preface
+  (defun LaTeX-insert-display-math ()
+    "Insert a \\=\\[ \\] block for display math."
+    (interactive)
+    (insert "\\[")
+    (indent-for-tab-command)
+    (newline-and-indent)
+    (newline-and-indent)
+    (insert "\\]")
+    (indent-for-tab-command)
+    (previous-line)
+    (indent-for-tab-command))
+
+  (defun my-TeX-mode-hook ()
+    (TeX-fold-mode 1)
+    (auto-fill-mode 1)
+    (setq-default TeX-PDF-mode t)
+    (setq-default TeX-auto-local "~/.cache/auctex/auto")
+    (setq-default TeX-auto-save t)
+    (setq-default TeX-parse-self t)
+    (bind-key "RET" 'indent-and-newline-and-indent TeX-mode-map))
+
+  (defun my-LaTeX-mode-hook ()
+    (setq-default LaTeX-default-environment "proof")
+    ;; http://tex.stackexchange.com/a/124259/24333
+    (setq-default LaTeX-command-style '(("" "%(PDF)%(latex) -file-line-error %S%(PDFout)")))
+    (bind-key "C-t" 'LaTeX-insert-display-math LaTeX-mode-map)
+
+    ;; preview-latex
+    (setq-default preview-auto-cache-preamble t)
+    (eval-after-load "preview"
+      '(add-to-list 'preview-default-preamble "\\PreviewEnvironment{tikzcd}" t)))
+
+  :config
+  (add-hook 'TeX-mode-hook 'my-TeX-mode-hook)
+  (add-hook 'LaTeX-mode-hook 'my-LaTeX-mode-hook)
+  (if (equal mytheme 'solarized-dark)
+      (eval-after-load 'font-latex
+        '(progn
+           (set-face-attribute 'font-latex-math-face nil :foreground "green")
+           (set-face-attribute 'font-latex-sedate-face nil :foreground "cyan")
+           (set-face-attribute 'font-latex-verbatim-face nil :foreground "white")))))
+
+(use-package auto-complete :ensure t :defer t)
+
+(use-package crontab-mode :ensure t :mode "/crontab\\(\\.[^/]*\\)?\\'")
+
+(use-package git-commit-mode :ensure t :defer t :config
+  (if (equal mytheme 'solarized-dark)
+      (set-face-attribute 'git-commit-summary-face nil :foreground "blue")))
+
+(use-package go-mode :ensure t :mode "\\.go\\'")
+
+(use-package jedi :ensure t :defer t)
+
+(use-package magit :ensure t :defer 3 :diminish magit-auto-revert-mode :init
+  (setq magit-last-seen-setup-instructions "1.4.0"))
+
+(use-package markdown-mode :ensure t :mode ("\\.md\\'" . gfm-mode) :config
+  (add-hook 'gfm-mode-hook 'abbrev-mode))
+
+(use-package markup-faces :ensure t :defer t :config
+  (if (equal mytheme 'solarized-dark)
+      (progn
+        (set-face-attribute 'markup-meta-face nil :foreground "green")
+        (set-face-attribute 'markup-meta-hide-face nil :foreground "brightgreen")
+        (set-face-attribute 'markup-reference-face nil :foreground "green")
+        (set-face-attribute 'markup-secondary-text-face nil :foreground "yellow")
+        (set-face-attribute 'markup-strong-face nil :foreground "red" :weight 'bold)
+        (set-face-attribute 'markup-title-0-face nil :foreground "blue" :weight 'bold)
+        (set-face-attribute 'markup-title-1-face nil :foreground "blue" :weight 'bold)
+        (set-face-attribute 'markup-typewriter-face nil :foreground "green"))))
+
+(use-package simple :diminish visual-line-mode)
+
+(use-package web-beautify :ensure t :defer 3)
+
+(use-package yaml-mode :ensure t :mode "\\.yml\\'")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; LOCAL PACKAGES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package pasteboard :defer 0
+  :bind (("C-c C-w" . pasteboard-copy)
+         ("C-c C-y" . pasteboard-paste)
+         ("C-c C-k" . pasteboard-cut))
+  :config
+  (if window-system
+      (progn
+        (isolate-kill-ring)
+        (bind-key "s-c" 'pasteboard-copy)
+        (bind-key "s-v" 'pasteboard-paste)
+        (bind-key "s-x" 'pasteboard-cut))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; AUTO INSERT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package autoinsert :defer 0
+  :preface
+  (auto-insert-mode)
+  (setq auto-insert-query nil)
+  (setq auto-insert-directory "~/.emacs.d/local/templates")
+
+  ;; LaTeX
+  (setq my-amsart-preamble-file
+        (substitute-in-file-name "$HOME/.emacs.d/local/templates/latex/amsart-preamble.tex"))
+  (define-auto-insert '("\.tex\\'" . "LaTeX skeleton")
+    '(nil
+      "% " (file-name-nondirectory (buffer-file-name)) \n
+      "%" \n
+      "% Created by Zhiming Wang on " (format-time-string "%B %d, %Y.") \n \n
+      "\\documentclass{amsart}" \n \n
+      "\\input{" my-amsart-preamble-file "}" \n \n
+      "\\title{" (read-string "title (if empty, do not make title): ") & ; if title is nonempty
+      (nil
+       "}" \n
+       "\\author{Zhiming Wang}" \n
+       "\\date{\\today}" \n \n
+       "\\begin{document}" \n \n
+       "\\maketitle" \n \n
+       _ \n \n
+       "\\end{document}"
+       )
+      | ; if title is empty
+      (nil
+       '(kill-whole-line -1) \n
+       "\\begin{document}" \n \n
+       _ \n \n
+       "\\end{document}")))
+
+  ;; Perl
+  (define-auto-insert '("\\.pl\\'" . "Perl skeleton") '(nil "#!/usr/bin/env perl" \n _ ))
+
+  ;; Python
+  (define-auto-insert '("\\.py\\'" . "Python skeleton") '(nil "#!/usr/bin/env python3" \n _ ))
+
+  ;; Ruby
+  (define-auto-insert '("\\.rb\\'" . "Ruby skeleton") '(nil "#!/usr/bin/env ruby" \n _ ))
+
+  ;; shell script
+  (define-auto-insert '("\\.\\(ba\\)?sh\\'" . "Bash skeleton") '(nil "#!/usr/bin/env bash" \n _ ))
+  (define-auto-insert '("\\.zsh\\'" . "Zsh skeleton") '(nil "#!/usr/bin/env zsh" \n _ )))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; WINDOW SPECIFIC ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(if (window-system)
+  (progn
+    ;; disable toolbar, tooltip, scrollbar, and window fringe
+    (tool-bar-mode -1)
+    (tooltip-mode -1)
+    (scroll-bar-mode -1)
+    (fringe-mode 0)
+    ;; set frame size for window-system
+    (add-to-list 'default-frame-alist (cons 'width 81))
+    (add-to-list 'default-frame-alist
+         (cons 'height (/ (- (x-display-pixel-height) 55)
+                          (frame-char-height))))))
---
+++ emacs/local/pasteboard.el
@@ -0,0 +1,63 @@
+;;; pasteboard.el --- Interact with OS X system pasteboard
+
+;; Copyright (C) 2015  Zhiming Wang
+
+;; Author: Zhiming Wang <zmwangx@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Remember to run `M-x isolate-kill-ring' in a window system to prevent the
+;; Emacs kill ring from picking up random stuff copied elsewhere.
+
+;;; Code:
+
+(defun isolate-kill-ring()
+  "Isolate Emacs kill ring from OS X system pasteboard.
+
+This function is only useful in window system."
+  (interactive)
+  (setq interprogram-cut-function nil)
+  (setq interprogram-paste-function nil))
+
+(defun pasteboard-copy()
+  "Save region to OS X system pasteboard."
+  (interactive)
+  (shell-command-on-region
+   (region-beginning) (region-end) "pbcopy")
+  (deactivate-mark))
+
+(defun pasteboard-paste()
+  "Paste from OS X system pasteboard via `pbpaste'.
+
+Windows CRLF or classic Mac OS CR are converted to Unix LF."
+  (interactive)
+  (shell-command-on-region
+   (point) (if mark-active (mark) (point))
+   "pbpaste | perl -p -e 's/\r$//' | tr '\r' '\n'" nil t))
+
+(defun pasteboard-cut()
+  "Kill region and save to OS X system pasteboard."
+  (interactive)
+  (pasteboard-copy)
+  (delete-region (region-beginning) (region-end)))
+
+(defalias 'pbcopy 'pasteboard-copy)
+(defalias 'pbpaste 'pasteboard-paste)
+(defalias 'pbcut 'pasteboard-cut)
+
+(provide 'pasteboard)
+;;; pasteboard.el ends here
