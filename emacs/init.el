4d1ac6200b6ba1573e6fe44c03ed516ba00b17e2
Zhiming Wang <zmwangx@gmail.com>
2015-07-31 08:27:25 -0700

    magit customizations: soft wrap and word diff

--- emacs/init.el
+++ emacs/init.el
@@ -286,7 +286,9 @@
   (setq-default js-indent-level 2)
   (setq-default json-reformat:indent-width 2))
 
-(use-package magit :ensure t :defer 0 :bind ("M-m" . magit-status))
+(use-package magit :ensure t :defer 0 :bind ("M-m" . magit-status) :config
+  (add-hook 'magit-mode-hook 'visual-line-mode)
+  (setq-default magit-diff-refine-hunk t))
 
 (use-package markdown-mode :ensure t :mode ("\\.md\\'" . gfm-mode) :config
   (add-hook 'gfm-mode-hook 'abbrev-mode))
