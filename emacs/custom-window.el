09b944901a9f41a60cae85ee7bd5556a0fd0b91c
Zhiming Wang <zmwangx@gmail.com>
2015-12-17 14:02:06 -0800

    emacs/custom-window.el: Use Monaco 11pt in window mode
    Not sure why the heck I chose Hack. Looks pretty bad. Monaco 11pt is not
    great, but at least better than Hack.

--- emacs/custom-window.el
+++ emacs/custom-window.el
@@ -20,4 +20,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 110 :family "Hack")))))
+ '(default ((t (:height 110 :family "Monaco")))))
