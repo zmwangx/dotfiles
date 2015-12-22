72f00a4813c518702087d9ffa5468881a00ced0d
Zhiming Wang <zmwangx@gmail.com>
2015-12-22 12:14:03 -0800

    wget: Add wgetrc, set WGETRC env var, and update alias
    Unfortunately, --hsts-file cannot be specified in wgetrc (at least not
    in wget 1.17.1), so we still need to have an alias.
    
    The `spider' alias for `wget --spider' has been removed since I don't
    use it that much, and the full command isn't hard to type.

--- zsh/extras/aliases.zsh
+++ zsh/extras/aliases.zsh
@@ -50,11 +50,10 @@ alias plk='parallel --keep-order'
 alias plx='parallel --xapply'
 alias plxk='parallel --xapply --keep-order'
 alias spellcheck='aspell check'
-alias spider='wget --spider'
 alias ss='date +%Y%m%d%H%M%S'
 alias tn='tmux new-window'
 alias visudo='VISUAL=vi sudo visudo'
-alias wget='wget --no-check-certificate --continue --header="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"'
+alias wget='wget --hsts-file=${XDG_DATA_HOME:-$HOME/.local/share}/wget/wget-hsts'
 alias zap='zap-DS_Store'
 
 ################################### OVERRIDE ###################################
