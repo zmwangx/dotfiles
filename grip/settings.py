457dd545f2263744905776e47fdc145322934c4a
Zhiming Wang <zmwangx@gmail.com>
2016-06-21 22:50:12 +0800

    grip/settings.py: Set up username and password (token)
    For higher rate limit.

--- grip/settings.py
+++ grip/settings.py
@@ -7,3 +7,10 @@ if 'XDG_CACHE_HOME' in os.environ:
 else:
     XDG_CACHE_HOME = os.path.expanduser('~/.cache')
 CACHE_DIRECTORY = os.path.join(XDG_CACHE_HOME, 'grip')
+
+here = os.path.dirname(os.path.abspath(__file__))
+tokenfile = os.path.join(here, 'token')
+if os.path.exists(tokenfile):
+    with open(tokenfile) as fp:
+        USERNAME = 'zmwangx'
+        PASSWORD = fp.read().strip()
